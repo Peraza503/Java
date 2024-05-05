adf8576b16cf6f58e3f7f5597388052317f67ea4
Peraza503 <166276529+Peraza503@users.noreply.github.com>
2024-05-04 23:01:58 -0600

    Add files via upload

---
+++ PP24012/src/main/java/PP24012/PP24012/App.java
@@ -0,0 +1,18 @@
+package PP24012.PP24012;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+	public String Hello() {
+		return "Hello Wordl";
+	}
+	
+    public static void main( String[] args )
+    {
+    	App hw= new App();
+        System.out.println( hw.Hello() );
+    }
+}
